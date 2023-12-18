02332a03bbaa90bd1c3d17e102902f36ce2b9896
rameezfayyaz <rameezfyyz@hotmail.com>
2023-12-18 21:47:04 +0100

    Additional materials UI Canvas made smaller for matching pixel size and aspect ratios

--- Assets/Scripts/ChangeWall.cs
+++ Assets/Scripts/ChangeWall.cs
@@ -2,23 +2,38 @@
 // using System.Collections.Generic;
 // using System.Linq;
 // using Microsoft.Unity.VisualStudio.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChangeWall : MonoBehaviour
 {
     public GameObject[] Walls;
+    public GameObject[] AlternateWalls;
     public Image[] buttonImages;
+    public Sprite[] FirstTexture, SecondTexture;
+    public Image[] ColorButtons;
+    public GameObject[] ColorSelectBar;
     public Color SelectedColor;
     public Color DeselectedColor;
-    public Material[] WallMaterials;
-    private int ActiveWall = 0;
+    private int ActiveWall = 0, ActiveColor = 0;
+    private bool changingWall = false;
+    private bool changingColor = false;
     public void WallSelected(int wallID) {
+        
+        if (changingWall) return;
+        if (wallID == ActiveWall) return;
+        changingWall = true;
+        Invoke("EnableWallSelection", 0.75f);
+
         for (int i=0; i<Walls.Length; i++) {
             if (i == wallID) {
+                // if (Walls[i].activeSelf) return;
                 Walls[i].SetActive(true);
                 buttonImages[i].color = SelectedColor;
                 ActiveWall = i;
+                ActiveColor = 0;
+                UpdateColors();
             }
             else {
                 Walls[i].SetActive(false);
@@ -26,8 +41,42 @@ public class ChangeWall : MonoBehaviour
             }
         }
     }
-    public void MaterialSelected(int materialID) {
-        Walls[ActiveWall].GetComponent<Transform>().GetChild(0)
-        .GetComponent<MeshRenderer>().material = WallMaterials[materialID];
+    private void UpdateColors() {
+        ColorButtons[0].sprite = FirstTexture[ActiveWall];
+        ColorButtons[1].sprite = SecondTexture[ActiveWall];
+    }
+    private void EnableWallSelection() {
+        changingWall = false;
+    }
+    private void EnableColorSelection() {
+        changingColor = false;
+    }
+    public void ColorSelected(int colorIndex) {
+        if (changingColor) return;
+        if (colorIndex == ActiveColor) return;
+        changingColor = true;
+        Invoke("EnableColorSelection", 0.75f);
+        switch (colorIndex) {
+            case 0:
+                Walls[ActiveWall].SetActive(true);
+                AlternateWalls[ActiveWall].SetActive(false);
+                ColorBar(true);
+            break;
+            case 1:
+                Walls[ActiveWall].SetActive(false);
+                AlternateWalls[ActiveWall].SetActive(true);
+                ColorBar(false);
+            break;
+            default: break;
+        }
+    }
+    private void ColorBar(bool first) {
+        if (first) {
+            ColorSelectBar[0].SetActive(true);
+            ColorSelectBar[1].SetActive(false);
+        } else {
+            ColorSelectBar[1].SetActive(true);
+            ColorSelectBar[0].SetActive(false);
+        }
     }
 }
--- Assets/Scripts/TouchHaptics.cs
+++ Assets/Scripts/TouchHaptics.cs
@@ -5,7 +5,6 @@ using UnityEngine.Events;
 
 public class TouchHaptics : MonoBehaviour
 {
-    public GameObject menuObject;
     public UnityEvent OnTouchRecorded;
     public bool disable = false;
     private bool running = false;
@@ -14,10 +13,6 @@ public class TouchHaptics : MonoBehaviour
             if (running) return;
             running = true;
             Debug.Log("Hand collided");
-            if (menuObject != null) {
-                SpriteAnimations[] anims = menuObject.GetComponents<SpriteAnimations>();
-                anims[0].ForceRestart();
-            }
             if (OnTouchRecorded != null) OnTouchRecorded.Invoke();
             if (disable) gameObject.SetActive(false);   
 
